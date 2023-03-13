fc93b5110d81847992c063451b8f1e39bdc60e3c
commuter-13 <118794280+commuter-13@users.noreply.github.com>
2023-03-13 20:36:36 +0800

    Add files via upload

--- 記帳.py
+++ 記帳.py
@@ -1,10 +1,18 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding="utf-8") as f:
+    for line in f:
+        name, price = line.strip().split(",")
+        products.append([name, price])
+print(products)
+
 
 while True:
     name = input("請輸入商品名稱：")
     if name == "q":
         break
     price = input("請輸入商品價格：")
+    price = int(price)
     products.append([name, price])
 
 print("完整清單:", products)
@@ -13,6 +21,7 @@ for p in products:
     print(p[0], "的價格是", p[1], "元")
 
 # 寫入檔案
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding="utf-8") as f:
+    # f.write("商品,價格\n")
     for p in products:
-        f.write(p[0]+','+p[1]+'\n')
+        f.write(p[0]+','+str(p[1])+'\n')
