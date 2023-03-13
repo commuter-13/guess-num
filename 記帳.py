98fafbcdb89dd816e265825f2fa35cb8ffb6e439
commuter-13 <118794280+commuter-13@users.noreply.github.com>
2023-03-13 22:28:00 +0800

    Add files via upload

--- 記帳.py
+++ 記帳.py
@@ -1,12 +1,19 @@
-# 讀取檔案
+import os
+
 products = []
-with open('products.csv', 'r', encoding="utf-8") as f:
-    for line in f:
-        if "商品,價格" in line:
-            continue
-        name, price = line.strip().split(",")
-        products.append([name, price])
-print(products)
+# 檢查檔案在不在
+if os.path.isfile("products.csv"):
+    print("開啟舊檔案")
+    # 讀取檔案
+    with open('products.csv', 'r', encoding="utf-8") as f:
+        for line in f:
+            if "商品,價格" in line:
+                continue
+            name, price = line.strip().split(",")
+            products.append([name, price])
+    print(products)
+else:
+    print("開啟新檔案")
 
 # 使用者輸入
 while True:
@@ -20,7 +27,17 @@ print("完整清單:", products)
 
 # 印出所有購買紀錄
 for p in products:
-    print(p[0], "的價格是", p[1], "元")
+    print(p[0], "價格", p[1], "元")
+
+# 印出總開銷
+price = []
+for p in products:
+    price.append(p[1])
+
+total = 0
+for i in price:
+    total += int(i)
+print("總共消費:", total, "元")
 
 # 寫入檔案
 with open('products.csv', 'w', encoding="utf-8") as f:
