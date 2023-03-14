873e4888fdb660c6009e656666fd7cff9f9bd57d
commuter-13 <118794280+commuter-13@users.noreply.github.com>
2023-03-14 14:45:16 +0800

    Add files via upload

--- 記帳.py
+++ 記帳.py
@@ -1,46 +1,58 @@
+# operating system
 import os
 
-products = []
-# 檢查檔案在不在
-if os.path.isfile("products.csv"):
-    print("開啟舊檔案")
-    # 讀取檔案
-    with open('products.csv', 'r', encoding="utf-8") as f:
+
+# 讀取檔案
+def read_file(fileName):
+    products = []
+    with open(fileName, 'r', encoding="utf-8") as f:
         for line in f:
             if "商品,價格" in line:
                 continue
             name, price = line.strip().split(",")
             products.append([name, price])
-    print(products)
-else:
-    print("開啟新檔案")
+    return products
 
-# 使用者輸入
-while True:
-    name = input("請輸入商品名稱：")
-    if name == "q":
-        break
-    price = input("請輸入商品價格：")
-    price = int(price)
-    products.append([name, price])
-print("完整清單:", products)
 
-# 印出所有購買紀錄
-for p in products:
-    print(p[0], "價格", p[1], "元")
+# 輸入商品
+def user_input(products):
+    while True:
+        name = input("請輸入商品名稱：")
+        if name == "q":
+            break
+        price = input("請輸入商品價格：")
+        price = int(price)
+        products.append([name, price])
+    print("完整清單:", products)
+    return products
 
-# 印出總開銷
-price = []
-for p in products:
-    price.append(p[1])
 
-total = 0
-for i in price:
-    total += int(i)
-print("總共消費:", total, "元")
+# 印出 所有購買紀錄
+def print_products(products):
+    for p in products:
+        print(p[0], "價格", p[1], "元")
+
 
 # 寫入檔案
-with open('products.csv', 'w', encoding="utf-8") as f:
-    f.write("商品,價格\n")
-    for p in products:
-        f.write(p[0]+','+str(p[1])+'\n')
+def write_file(fileName, products):
+    with open(fileName, 'w', encoding="utf-8") as f:
+        f.write("商品,價格\n")
+        for p in products:
+            f.write(p[0]+','+str(p[1])+'\n')
+
+
+# 主要執行
+def main():
+    fileName = "products.csv"
+    if os.path.isfile(fileName):
+        print("找到檔案，開啟舊檔案")
+        products = read_file(fileName)
+    else:
+        products = []
+        print("開啟新檔案")
+    products = user_input(products)
+    print_products(products)
+    write_file("products.csv", products)
+
+
+main()
