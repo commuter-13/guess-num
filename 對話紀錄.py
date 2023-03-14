176da4796dfa246508f852e7c0e9d035748a2e18
commuter-13 <118794280+commuter-13@users.noreply.github.com>
2023-03-14 18:48:12 +0800

    Add files via upload

---
+++ 對話紀錄.py
@@ -0,0 +1,42 @@
+# 讀取檔案
+def read_file(fileName):
+    lines = []
+    with open(fileName, "r", encoding="utf-8-sig")as f:
+        for line in f:
+            lines.append(line.strip())
+        return lines
+
+
+# 轉換
+def convert(lines):
+    new = []
+    # 預設值
+    person = None
+    for line in lines:
+        if line == "Allen":
+            person = "Allen"
+            continue
+        elif line == "Tom":
+            person = "Tom"
+            continue
+        if person:
+            new.append(person + ": " + line)
+    return new
+
+
+# 寫入檔案
+def write_file(fileName, lines):
+    with open(fileName, "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line + "\n")
+
+
+# 主要
+def main():
+    lines = read_file("input.txt")
+    print(lines)
+    lines = convert(lines)
+    write_file("output.txt", lines)
+
+
+main()
