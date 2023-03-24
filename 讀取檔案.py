4ec8098e49432faf36003169632bf13e33ee98d7
commuter-13 <118794280+commuter-13@users.noreply.github.com>
2023-03-24 15:53:45 +0800

    Add files via upload

---
+++ 讀取檔案.py
@@ -0,0 +1,62 @@
+# 讀取檔案
+data = []
+count = 0
+with open("reviews.txt", "r")as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 500000 == 0:
+            print(len(data))
+print("檔案讀取完了，總共有", len(data), "筆資料")
+print("第0筆的資料內容: ", data[0])
+
+# 算每筆資料的平均長度
+sum_len = 0
+for x in data:
+    sum_len += len(x)
+print("平均長度為", sum_len/len(data))
+
+# 篩選 資料的長度
+new = []
+for x in data:
+    if len(x) < 100:
+        new.append(x)
+print("一共有", len(new), "筆留言的長度小於100")
+print(new[0])
+
+# good
+good = []
+for x in data:
+    if "good" in x:
+        good.append(x)
+print("一共有", len(good), "筆留言提到good")
+print(good[0])
+
+
+# 文字計數
+wc = {}
+for d in data:
+    # split()預設值是空白鍵
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            # 新增新的key進wc字典
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, "出現的次數為", wc[word], "次")
+
+print("總共有幾個字:", len(wc))
+
+while True:
+    word = input("輸入想查詢的字:")
+    if word == "q":
+        break
+    if word in wc:
+        print(word, "出現的次數為", wc[word], "次")
+    else:
+        print("抱歉，查不到這個字")
+print("結束")
